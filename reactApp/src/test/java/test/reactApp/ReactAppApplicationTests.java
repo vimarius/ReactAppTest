5c90dde19c11c5e2734a05f3f4e69f119961d0d6
Marius <marius.vinogradovas@stud.vtmc.lt>
2021-03-29 13:32:37 +0300

    Added SQLite database file

---
+++ reactApp/src/test/java/test/reactApp/ReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package test.reactApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
