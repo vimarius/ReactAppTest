d3bf679879dd1eecdd969f1b1e3a6411e9d2c2f8
Marius <marius.vinogradovas@stud.vtmc.lt>
2021-03-30 13:41:36 +0300

    Added createStudentComponent

---
+++ react_app_test/src/components/CreateStudentComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class CreateStudentComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Add Student</h2>
+                
+            </div>
+        );
+    }
+}
+
+
+
+export default CreateStudentComponent;
