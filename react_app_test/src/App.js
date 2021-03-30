a7ddc3add8c27f1a8a351c0c10317f238bef8d0d
Marius <marius.vinogradovas@stud.vtmc.lt>
2021-03-30 11:17:26 +0300

    Added routing

--- react_app_test/src/App.js
+++ react_app_test/src/App.js
@@ -2,15 +2,25 @@ import './App.css';
 import StudentListComponent from './components/StudentListComponent';
 import HeaderComponent from './components/HeaderComponent.js';
 import FooterComponent from './components/FooterComponent.js';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <Router>
+        {/* <div className="container"> */}
       <HeaderComponent />
       <div className="container">
+        <Switch> 
+          <Route path="/" component={StudentListComponent}></Route>
+          <Route path="/students" component={StudentListComponent}></Route>
       <StudentListComponent />
+      </Switch>
+      
     </div>
     <FooterComponent />
+    {/* </div> */}
+    </Router>
     </div>
   );
 }
